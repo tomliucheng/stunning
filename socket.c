d948ac2c19fd76fbf2a94017430c93f9a8f0821d
tomliucheng <tom.liucheng@aliyun.com>
2016-10-10 23:11:34 +0800

    Create socket.c
    about communication

---
+++ socket.c
@@ -0,0 +1,6 @@
+AF 表示ADDRESS FAMILY 地址族，PF 表示PROTOCOL FAMILY 协议族 这两个宏定义是一样的?
+windows cmd:getmac 获得48bits的macd地址信息
+
+LTE（Long Term Evolution，长期演进)是由
+3GPP（The 3rd Generation Partnership Project，第三代合作伙伴计划）组织制定的
+UMTS（Universal Mobile Telecommunications System，通用移动通信系统）技术标准的长期演进，于2004年12月在3GPP多伦多会议上正式立项并启动。
